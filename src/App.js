fbe5f453fcd5dc6372b9fb679b88cc71c9c0ed45
mhnarfth <mhnarfth@gmail.com>
2022-07-03 02:38:58 +0600

    header-and-router-added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import Restaurant from './component/Restaurant/Restaurant';
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import Restaurant from "./component/Restaurant/Restaurant";
+import Home from "./component/Home/Home";
+import About from "./component/About/About";
+import Header from "./component/Header/Header";
 
 function App() {
   return (
-    <div className="App">
-      <Restaurant></Restaurant>
-    </div>
+    <>
+      <div className="App">
+        <Header></Header>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="restaurant" element={<Restaurant />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </>
   );
 }
 
--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,7 +1,50 @@
+import { Home } from "@mui/icons-material";
+import { red } from "@mui/material/colors";
+import { fontSize } from "@mui/system";
 import React from "react";
+import { NavLink } from "react-router-dom";
+import About from "../About/About";
+import Restaurant from "../Restaurant/Restaurant";
+import "./Header.css";
 
 const Header = () => {
-  return <div></div>;
+  let activeStyle = {
+    color: "red",
+  };
+
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <NavLink
+              className="navlink-style"
+              to="/"
+              style={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className="navlink-style"
+              to="restaurant"
+              style={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
+              Restaurant
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="navlink-style" to="about">
+              {({ isActive }) => (
+                <span style={isActive ? activeStyle : undefined}>About</span>
+              )}
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
