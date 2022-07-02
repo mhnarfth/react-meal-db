8a464e3e2ec22245de1a0c92b43a7b09d5df4cdc
mhnarfth <mhnarfth@gmail.com>
2022-07-03 01:16:44 +0600

    material-ui-card-added-and-showed-in-Meal-component

--- src/component/Restaurant/Restaurant.js
+++ src/component/Restaurant/Restaurant.js
@@ -1,33 +1,37 @@
-import { TextField } from '@mui/material';
-import React, { useEffect, useState } from 'react';
+import { TextField } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import Meal from "../Meal/Meal";
 
 const Restaurant = () => {
+  const [searchText, setSearchText] = useState("");
+  const [meals, setMeals] = useState([]);
 
-    const [searchText, setSearchText] = useState('');
-    const [meal, setMeal] = useState([]);
+  const handleSearchField = (e) => {
+    const searchText = e.target.value;
+    setSearchText(searchText);
+  };
 
-    const handleSearchField = e => {
-        const searchText = e.target.value;
-        setSearchText(searchText);
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
 
-    }
-    
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setMeals(data.meals));
+  }, [searchText]);
 
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
-
-    useEffect(()=> {
-        fetch (url)
-        .then (res=> res.json())
-        .then (data=> setMeal(data));
-    }, [searchText]);
-    console.log(meal);
-
-
-    return (
-        <div>
-            <TextField onChange={handleSearchField} id="filled-basic" label="Enter Desired Meal" variant="filled" />
-        </div>
-    );
+  return (
+    <div>
+      <TextField
+        onChange={handleSearchField}
+        id="filled-basic"
+        label="Enter Desired Meal"
+        variant="filled"
+      />
+      {meals.map((meal) => (
+        <Meal key={meal.idMeal} meal={meal}></Meal>
+      ))}
+    </div>
+  );
 };
 
 export default Restaurant;
